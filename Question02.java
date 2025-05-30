1806ccbcd38adf2825c424b6d6049660dc923a84
Nikeshala19 <nikeshala.r@outlook.com>
2025-05-30 21:48:45 +0000

    modifies

--- Question02.java
+++ Question02.java
@@ -1,4 +1,4 @@
-public class Vehicle {
+class Vehicle {
     private String licensePlate;
     private String ownerName;
     private int hoursParked;
@@ -40,20 +40,35 @@ class ParkingLot {
     private Vehicle[] vehicles = new Vehical[5];
     private int count = 0;
 
-    
-    }
-
     public void parkVehicle(Vehicle vehicle) {
-        
+        if (count < vehicles.length) {
+            vehicles[count++] = vehicle;
+        } else {
+            System.out.println("Parking lot is full.");
+        }
     }
 
     public void removeVehicle(String licensePlate) {
-        
+          for (int i = 0; i < count; i++) {
+            if (vehicles[i].getLicensePlate().equals(licensePlate)) {
+                for (int j = i; j < count - 1; j++) {
+                    vehicles[j] = vehicles[j + 1];
+                }
+                vehicles[--count] = null;
+                return;
+            }
         }
+        System.out.println("Vehicle not found.");
+    }
 
 
     public void displayVehicles() {
-      
+       for (int i = 0; i < count; i++) {
+            System.out.println("License: " + vehicles[i].getLicensePlate() +
+                               ", Owner: " + vehicles[i].getOwnerName() +
+                               ", Hours: " + vehicles[i].getHoursParked());
+        }
+    }
 }
 
 public class Question02 {
